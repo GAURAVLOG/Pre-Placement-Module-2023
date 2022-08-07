8e51438d6975945686cba00120db6057e688833e
GAURAVLOG <75735081+GAURAVLOG@users.noreply.github.com>
2022-08-07 17:19:26 +0530

    Update 1.cpp

--- ALGO II/Day 1/1.cpp
+++ ALGO II/Day 1/1.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        
-        for(vector<int> &i:matrix){
-            reverse(i.begin(),i.end());
-        }
-        int j = matrix.size()-1;
-        for(int i = 0; i<matrix.size(); i++,j--){
-            int k = 0;
-            int l = matrix.size()-1;
-            while(k<matrix.size()-i){
-                swap(matrix[i][k],matrix[l][j]);
-                k++;
-                l--;
+    int first(vector<int>& nums, int target){
+        int i = 0, j = nums.size()-1 ;
+        int ans = -1;
+        while(i <= j){
+            int mid = i + ((j-i)/2);
+            if (nums[mid]==target){
+                ans = mid;
+                j = mid-1;
             }
+            else if (nums[mid]<target) i = mid+1;
+            else j = mid-1;
         }
+        return ans;
+    }
+    int last(vector<int>& nums, int target){
+        int i = 0, j = nums.size()-1 ;
+        int ans = -1;
+        while(i <= j){
+            int mid = i + ((j-i)/2);
+            if (nums[mid]==target){
+                ans = mid;
+                i = mid+1;
+            }
+            else if (nums[mid]<target) i = mid+1;
+            else j = mid-1;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans(2);
+        ans[0] = first(nums,target);
+        ans[1] = last(nums,target);
+        
+        return ans;
     }
 };
